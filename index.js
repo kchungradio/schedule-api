7a6a2fb32a1a2c69b95a3c7e13223c30306875c2
Tim Phillips <tim.p.phillips@gmail.com>
2017-02-15 19:30:48 -0800

    adjust for timezone difference on server

--- index.js
+++ index.js
@@ -3,36 +3,6 @@ const url = require('url')
 const jwt = require('./lib/auth/jwt')
 const calendars = require('./lib/calendars')
 
-async function fetchEvents (date) {
-  let googleEvents
-
-  try {
-    await calendars.auth(jwt)
-  } catch (err) {
-    err.statusCode = 400
-    throw err
-  }
-
-  try {
-    googleEvents = await calendars
-      .getEvents(date.getFullYear(), date.getMonth() + 1)
-  } catch (err) {
-    err.statusCode = 400
-    throw err
-  }
-
-  const events = googleEvents.map(event => ({
-    id: event.id,
-    name: event.summary,
-    start: event.start.dateTime,
-    end: event.end.dateTime
-  })).filter(event => {
-    return !(event.name.includes('TENTATIVE') || event.name.includes('TBD'))
-  })
-
-  return events
-}
-
 module.exports = async function (req, res) {
   let pathname, date, events, year, month, err
 
@@ -48,14 +18,15 @@ module.exports = async function (req, res) {
   if (pathname.length !== 3) {
     // get current year and month
     const now = new Date()
-    year = now.getFullYear()
-    month = now.getMonth() + 1
+
+    year = now.getUTCFullYear()
+    month = now.getUTCMonth() + 1
   } else {
     year = pathname[1]
     month = pathname[2]
   }
 
-  date = new Date(year, month - 1)
+  date = new Date(Date.UTC(year, month - 1))
 
   if (isNaN(date.getTime())) {
     err = new Error('Not a date')
@@ -69,6 +40,37 @@ module.exports = async function (req, res) {
     err.statusCode = 400
     throw err
   }
+
+  return events
+}
+
+async function fetchEvents (date) {
+  // TODO: put this function in lib/calendar?
+  let googleEvents
+
+  try {
+    await calendars.auth(jwt)
+  } catch (err) {
+    err.statusCode = 400
+    throw err
+  }
+
+  try {
+    googleEvents = await calendars
+      .getEvents(date.getUTCFullYear(), date.getUTCMonth() + 1)
+  } catch (err) {
+    err.statusCode = 400
+    throw err
+  }
+
+  const events = googleEvents.map(event => ({
+    id: event.id,
+    name: event.summary,
+    start: event.start.dateTime,
+    end: event.end.dateTime
+  })).filter(event => {
+    return !(event.name.includes('TENTATIVE') || event.name.includes('TBD'))
+  })
 
   return events
 }
