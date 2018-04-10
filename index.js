d389271e9a603f73c8d2847ae4a5b922c004c4ec
Tim Phillips <tim.p.phillips@gmail.com>
2018-04-09 17:32:23 -0700

    parse description, converting html to text

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const url = require('url')
 const validator = require('validator')
+const htmlToText = require('html-to-text')
 
 const jwt = require('./lib/auth/jwt')
 const calendars = require('./lib/calendars')
@@ -76,9 +77,14 @@ async function fetchEvents (date) {
     // find url in description
     let url
     if (event.description) {
-      url = event.description.split('\n').filter(line =>
-        validator.isURL(line) && line.indexOf('@') === -1
-      )[0]
+      const parsedDescription = htmlToText.fromString(event.description, {
+        preserveNewlines: true,
+        hideLinkHrefIfSameAsText: true
+      })
+      url = parsedDescription.split('\n').filter(line => {
+        // is url and is not email address
+        return validator.isURL(line) && line.indexOf('@') === -1
+      })[0]
       if (url) sanitizedEvent.url = url
     }
 
