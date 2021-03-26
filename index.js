7550e467fbfdde38c9d87ed7f4eef36e98ca911b
Tim Phillips <tim.p.phillips@gmail.com>
2021-03-25 17:00:35 -0700

    list emails

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const url = require('url')
 
 const { createError } = require('micro')
+const htmlToText = require('html-to-text')
+const getEmails = require('get-emails')
 
 const fetchEvents = require('./lib/fetch-events')
 const sanitizeEvent = require('./lib/sanitize-event')
@@ -12,6 +14,9 @@ module.exports = async function(req, res) {
 
   // get the pathname "/year/month" from the url as ['', year, month]
   const pathname = url.parse(req.url).pathname.split('/')
+  if (pathname[1].includes('favicon')) {
+    return 'fav'
+  }
 
   // set the year and month that we'll use to get events
   let year, month
@@ -44,6 +49,56 @@ module.exports = async function(req, res) {
     throw err
   }
 
+  return printEmails(events)
+  // printCsv(events)
+
+  return 'done'
+
   // return the events as a json 200 OK response
   return events.map(sanitizeEvent).filter(filterEvent)
+}
+
+function printEmails(events) {
+  let allEmails = []
+
+  events.forEach(event => {
+    const parsedDescription = htmlToText.fromString(event.description, {
+      preserveNewlines: true,
+      hideLinkHrefIfSameAsText: true
+    })
+
+    const emails = Array.from(getEmails(parsedDescription))
+    if (emails && emails.length) {
+      allEmails = allEmails.concat(emails)
+    }
+  })
+
+  const emailSet = new Set(allEmails)
+  const emailArray = Array.from(emailSet)
+
+  emailArray.forEach(email => {
+    console.log(email)
+  })
+
+  return emailArray.join('\n')
+}
+
+function printCsv(events) {
+  events.forEach(event => {
+    const parsedDescription = htmlToText.fromString(event.description, {
+      preserveNewlines: true,
+      hideLinkHrefIfSameAsText: true
+    })
+
+    let str = `"${event.summary}", `
+
+    str = str + `"${parsedDescription}", `
+
+    const emails = Array.from(getEmails(parsedDescription))
+    if (emails && emails.length) {
+      str = str + emails.join(', ')
+    }
+
+    console.log(str)
+  })
 }
